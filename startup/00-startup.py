f8d107d6b975aa8eda9847cc53d71490dfe168c2
Stuart Campbell <stuart@stuartcampbell.me>
2018-04-14 23:38:12 -0400

    Create 00-startup.py
    Initial version of the startup file.

---
+++ startup/00-startup.py
@@ -0,0 +1,33 @@
+import nslsii
+nslsii.configure_base(get_ipython().user_ns, 'sst')
+
+# After the above call, you will now have the following in your namespace:
+# 
+#	RE : RunEngine 
+#	db : databroker 
+#	sd : SupplementalData
+#	pbar_manager : ProgressBarManager
+#	bec : BestEffortCallback
+#	peaks : bec.peaks
+#	plt : matplotlib.pyplot
+#	np : numpy
+#	bc : bluesky.callbacks
+#	bp : bluesky.plans
+#	bps : bluesky.plan_stubs
+#	mv : bluesky.plan_stubs.mv
+#	mvr : bluesky.plan_stubs.mvr
+#	mov : bluesky.plan_stubs.mov
+#	movr : bluesky.plan_stubs.movr
+#	bpp : bluesky.preprocessors
+
+# Optional: set any metadata that rarely changes.
+# RE.md['beamline_id'] = 'YOUR_BEAMLINE_HERE'
+
+# Add a callback that prints scan IDs at the start of each scan.
+def print_scan_ids(name, start_doc):
+    print("Transient Scan ID: {0} @ {1}".format(start_doc['scan_id'],time.strftime("%Y/%m/%d %H:%M:%S")))
+    print("Persistent Unique Scan ID: '{0}'".format(start_doc['uid']))
+
+RE.subscribe(print_scan_ids, 'start')
+
+
