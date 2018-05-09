608878908abfd06a54f1cbb1b8d5ef30f35aaf72
Stuart Campbell <stuart@stuartcampbell.me>
2018-05-08 21:04:53 -0400

    Add some EpicsSignals to read

---
+++ startup/50-signals.py
@@ -0,0 +1,13 @@
+from ophyd import EpicsSignalRO
+
+# These might need/make more sense to be split up into separate files later on.
+# But while we have so few, I'm just putting them in this single file.
+
+bpm13_sum = EpicsSignalRO('XF:07ID-BI{BPM:13}Stats5:Total_RBV', name='bpm13_sum')
+
+dm3_c1 = EpicsSignalRO('XF:07ID-BI{DM3:I400-1}:IC1_MON', name='dm3_c1')
+
+# Not sure how best to do this image yet... 
+
+#BPM13 Image:
+#XF:07ID-BI{BPM:13}image1:ArrayData
