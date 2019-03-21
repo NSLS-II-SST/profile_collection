0f14c03a7d1d6e996c46781f96d914879ae7ba30
RSoXS <egann@bnl.gov>
2019-03-21 12:12:07 -0400

    Added energy signal to be used along with the ADs.

--- startup/50-signals.py
+++ startup/50-signals.py
@@ -1,6 +1,6 @@
 print(f'Loading {__file__}...')
 
-from ophyd import EpicsSignalRO
+from ophyd import EpicsSignal, EpicsSignalRO
 
 # These might need/make more sense to be split up into separate files later on.
 # But while we have so few, I'm just putting them in this single file.
@@ -8,6 +8,10 @@ from ophyd import EpicsSignalRO
 bpm13_sum = EpicsSignalRO('XF:07ID-BI{BPM:13}Stats5:Total_RBV', name='bpm13_sum')
 
 dm3_c1 = EpicsSignalRO('XF:07ID-BI{DM3:I400-1}:IC1_MON', name='dm3_c1')
+
+energy = EpicsSignal(read_pv='XF:07ID1-OP{Mono:PGM1-Ax::ENERGY_MON',
+                     write_pv='XF:07ID1-OP{Mono:PGM1-Ax::ENERGY_SP',
+                     name='energy')
 
 # Not sure how best to do this image yet... 
 
