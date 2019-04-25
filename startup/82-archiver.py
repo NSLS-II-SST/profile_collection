519571c00dfabace8fa5282096b6975868288a77
RSoXS <egann@bnl.gov>
2019-04-25 10:25:06 -0400

    Refining Archiver plotting

--- startup/82-archiver.py
+++ startup/82-archiver.py
@@ -16,13 +16,8 @@ class plotpvs(list):
 
     def __init__(self, pvdict = {}, hours = 0, minutes = 0, seconds = 0, days = 0, weeks = 0):
         list.__init__(self)
-        self.figure = plt.figure('Archiver Plot')
         until = datetime.today()
         since = until - timedelta(days=days, seconds=seconds,  minutes=minutes, hours=hours, weeks=weeks)
-        #since = datetime(2019, 1, 1)
-        #until = datetime(2019, 2, 12)
-
-        # request data from archiver
 
         since = pytz.timezone(self.timezone).localize(since).replace(microsecond=0).isoformat()
         until = pytz.timezone(self.timezone).localize(until).replace(microsecond=0).isoformat()
@@ -52,16 +47,21 @@ class plotpvs(list):
             df[pvname] = pd.to_numeric(df[pvname],errors='coerce')
             df[pvname+'t'] = df[pvname+'t'].dt.tz_localize('UTC').dt.tz_convert(self.timezone)
             df = df.set_index(pvname+'t')
-            #df.plot(pvname+'t',pvname,logy=1,linewidth=.2,figure=self.figure)
             if weeks:
                 df = df.resample('1H').mean()
             elif days:
-                df = df.resample('10M').mean()
+                df = df.resample('10T').mean()
             elif hours:
                 df = df.resample('10S').mean()
             else:
                 df = df.resample('1S').mean()
+            df = df.ffill()
             self.append(df)
+        self.plot()
+    def plot(self):
+        self.figure = plt.figure('Archiver Plot')
+        ax1 = plt.gca()
+        for df in self:
+            df.plot(ax=ax1)
 
-pvdictionary = {'Main Chamber Pressure':'XF:07IDB-VA:2{RSoXS:Main-CCG:1}P:Raw-I'}
-pvdictionary['Load Lock Pressure']='XF:07IDB-VA:2{RSoXS:LL-CCG:1}P:Raw-I'
+
