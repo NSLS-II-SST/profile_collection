cacf70341ca3535df52ccf1c8fff598f07829271
RSoXS <egann@bnl.gov>
2019-08-01 10:25:31 -0400

    Adding in dark image module

--- startup/80-export.py
+++ startup/80-export.py
@@ -26,44 +26,50 @@ USERDIR = 'Z:/images/users/'
 
 def factory(name, start_doc):
     filler = Filler(db.reg.handler_reg)
-
+    dt = datetime.now()
+    formatted_date = dt.strftime('%Y-%m-%d')
     # filler(name, start_doc)  # modifies doc in place
+    SAXSsubtractor = DarkSubtraction('Synced_saxs_image')
+    WAXSsubtractor = DarkSubtraction('Synced_waxs_image')
+    SAXSserializer = tiff_series.Serializer(file_prefix=('{start[institution]}/'
+                                                         '{start[user_name]}/'
+                                                         '{start[project_name]}/'
+                                                         f'{formatted_date}/sd'
+                                                         '{start[scan_id]}-'
+                                                         '{start[sample_name]}-'
+                                                         ),
+                                            directory=USERDIR)
+
+    WAXSserializer = tiff_series.Serializer(file_prefix=('{start[institution]}/'
+                                                         '{start[user_name]}/'
+                                                         '{start[project_name]}/'
+                                                         f'{formatted_date}/wd'
+                                                         '{start[scan_id]}-'
+                                                         '{start[sample_name]}-'),
+                                            directory=USERDIR)
+
+    serializercsv = csv.Serializer(file_prefix=('{start[institution]}/'
+                                                '{start[user_name]}/'
+                                                '{start[project_name]}/'
+                                                f'{formatted_date}/'
+                                                '{start[scan_id]}-'
+                                                '{start[sample_name]}-'
+                                                # '{event[data][Beamline Energy_energy]:.2f}eV-'
+                                                ),
+                                   directory=USERDIR,
+                                   flush=True,
+                                   line_terminator='\n')
+
+    def fill_subtract_and_serialize(name, doc):
+        name, doc = filler(name, doc)
+        sname, sdoc = SAXSsubtractor(name, doc)
+        SAXSserializer(sname, sdoc)
+        wname, wdoc = WAXSsubtractor(name, doc)
+        WAXSserializer(wname, wdoc)
 
     def subfactory(name, descriptor_doc):
 
         if descriptor_doc['name'] in ['primary', 'dark']:
-            dt = datetime.now()
-            formatted_date = dt.strftime('%Y-%m-%d')
-            # #energy = hdr.table(stream_name='baseline')['Beamline Energy_energy'][1]
-            # serializer = tiff_series.Serializer(file_prefix=('{start[institution]}/'
-            #                                                  '{start[user_name]}/'
-            #                                                  '{start[project_name]}/'
-            #                                                  f'{formatted_date}/'
-            #                                                  '{start[scan_id]}-'
-            #                                                  '{start[sample_name]}-'
-            #                                                  #'{event[data][en_energy]:.2f}eV-'
-            #                                                  ),
-            #                                     directory=USERDIR)
-            # serializer('start', start_doc)
-            # serializer('descriptor', descriptor_doc)
-            SAXSsubtractor = DarkSubtraction('Synced_saxs_image')
-            WAXSsubtractor = DarkSubtraction('Synced_waxs_image')
-            SAXSserializer = tiff_series.Serializer(file_prefix=('{start[institution]}/'
-                                                                 '{start[user_name]}/'
-                                                                 '{start[project_name]}/'
-                                                                 f'{formatted_date}/sd'
-                                                                 '{start[scan_id]}-'
-                                                                 '{start[sample_name]}-'
-                                                                 ),
-                                                    directory=USERDIR)
-            WAXSserializer = tiff_series.Serializer(file_prefix=('{start[institution]}/'
-                                                                 '{start[user_name]}/'
-                                                                 '{start[project_name]}/'
-                                                                 f'{formatted_date}/wd'
-                                                                 '{start[scan_id]}-'
-                                                                 '{start[sample_name]}-'),
-                                                    directory=USERDIR)
-
             # Here we push the run 'start' doc through.
             SAXSsubtractor('start', start_doc)
             SAXSsubtractor('descriptor', descriptor_doc)
@@ -74,26 +80,7 @@ def factory(name, start_doc):
             WAXSserializer('start', start_doc)
             WAXSserializer('descriptor', descriptor_doc)
 
-            # And by returning this function below, we are routing all other
-            # documents *for this run* through here.
-            def fill_subtract_and_serialize(name, doc):
-                name, doc = filler(name, doc)
-                sname, sdoc = SAXSsubtractor(name, doc)
-                SAXSserializer(sname, sdoc)
-                wname, wdoc = WAXSsubtractor(name, doc)
-                WAXSserializer(wname, wdoc)
             if descriptor_doc['name'] == 'primary':
-                serializercsv = csv.Serializer(file_prefix=('{start[institution]}/'
-                                                            '{start[user_name]}/'
-                                                             '{start[project_name]}/'
-                                                            f'{formatted_date}/'
-                                                            '{start[scan_id]}-'
-                                                            '{start[sample_name]}-'
-                                                            #'{event[data][Beamline Energy_energy]:.2f}eV-'
-                                                            ),
-                                               directory=USERDIR,
-                                               flush=True,
-                                               line_terminator='\n')
                 serializercsv('start', start_doc)
                 serializercsv('descriptor', descriptor_doc)
                 return [fill_subtract_and_serialize,serializercsv]  # fill_subtract_and_serialize
@@ -116,15 +103,10 @@ def factory(name, start_doc):
             serializer('start', start_doc)
             serializer('descriptor', descriptor_doc)
             return [serializer]
-
         else:
             return []
 
-    def cb(name, doc):
-        filler(name, doc)  # Fill in place any externally-stored data written by area detector.
-
-
-    return [cb], [subfactory]
+    return [], [subfactory]
 
 
 rr = RunRouter([factory])
