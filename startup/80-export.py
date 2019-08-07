7c6ea3438a6e039c6be31ca9d75c5c313a440a0b
RSoXS <egann@bnl.gov>
2019-08-07 16:56:25 -0400

    no export on start doc

--- startup/80-export.py
+++ startup/80-export.py
@@ -37,7 +37,7 @@ def factory(name, start_doc):
                                                        f'{formatted_date}/'
                                                        '{start[scan_id]}-'
                                                        '{start[sample_name]}-'
-                                                       '{event[data][en_energy]:.2f}eV-'
+                                                       #'{event[data][en_energy]:.2f}eV-'
                                                        ),
                                           directory=USERDIR)
 
