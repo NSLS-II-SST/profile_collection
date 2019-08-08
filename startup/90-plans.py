51f20b63a51f5bbb3bfb6fbb4ebac7490fef3f69
RSoXS <egann@bnl.gov>
2019-08-08 16:13:59 -0400

    taking event energy out of suitcase cause it doesn't work anymore

--- startup/90-plans.py
+++ startup/90-plans.py
@@ -95,8 +95,8 @@ def dark_plan():
 
 
 dark_frame_preprocessor = bluesky_darkframes.DarkFramePreprocessor(
-    dark_plan=dark_plan, max_age=100, locked_signals=[sw_det.saxs.cam.acquire_time,Det_S, Det_W],
-    limit=10)
+    dark_plan=dark_plan, max_age=1000, locked_signals=[sw_det.saxs.cam.acquire_time,Det_S, Det_W],
+    limit=50)
 RE.preprocessors.append(dark_frame_preprocessor)
 # not doing this because EVERYTHING that goes through RE will get a dark image - this is excessive
 
