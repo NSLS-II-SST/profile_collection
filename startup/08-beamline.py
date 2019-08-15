86a26ec2a6a4402a3f218267b45a344d646eaa53
RSoXS <egann@bnl.gov>
2019-08-15 09:45:59 -0400

    changing light behavio, removing light object

--- startup/08-beamline.py
+++ startup/08-beamline.py
@@ -195,4 +195,4 @@ class PDU(EpicsSignal):
 # light = PDU('XF:07ID-CT{RG:C1-PDU:1}Sw:8-SP',write_pv='XF:07ID-CT{RG:C1-PDU:1}Sw:8-Sel')
 
 
-sd.baseline.extend([ccg_izero,pg_izero,ccg_main,pg_main,ccg_ll,pg_ll,ll_gpwr,psh1,psh4,psh10,psh7,gv14,gv14a,gv15,gv26,gv27,gv27a,gv28,gvTEM,gvll,gvturbo,light])
+sd.baseline.extend([ccg_izero,pg_izero,ccg_main,pg_main,ccg_ll,pg_ll,ll_gpwr,psh1,psh4,psh10,psh7,gv14,gv14a,gv15,gv26,gv27,gv27a,gv28,gvTEM,gvll,gvturbo])
