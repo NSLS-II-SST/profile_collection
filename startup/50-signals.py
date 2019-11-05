8a8139a3814191b9543c53e76731e7d8d218c4d8
RSoXS <egann@bnl.gov>
2019-11-05 15:49:05 -0500

    Trying a rebuild of I400 using areadetector following QuadEM example

--- startup/50-signals.py
+++ startup/50-signals.py
@@ -2,7 +2,10 @@ run_report(__file__)
 
 import time
 from ophyd import (EpicsSignal, EpicsSignalRO, EpicsSignalWithRBV, Device, Component, DeviceStatus,
-                   Staged)
+                   Staged, Signal)
+from ophyd.areadetector import (ADComponent as ADCpt, EpicsSignalWithRBV, ImagePlugin, StatsPlugin, DetectorBase,
+                                SingleTrigger, ADBase)
+from ophyd.status import DeviceStatus
 
 # These might need/make more sense to be split up into separate files later on.
 # But while we have so few, I'm just putting them in this single file.
@@ -12,13 +15,17 @@ bpm13_sum = EpicsSignalRO('XF:07ID-BI{BPM:13}Stats5:Total_RBV', name='Downstream
 ring_current = EpicsSignalRO('SR:OPS-BI{DCCT:1}I:Real-I', name='NSLS-II Ring Current', kind='normal')
 
 
-class I400(Device):
-    gain = Component(EpicsSignal, ':RANGE_BP')
-    exposure_time = Component(EpicsSignal, ':PERIOD_SP',put_complete=True)
-    acquisition_mode = Component(EpicsSignal, ':GETCS.SCAN')
-    acquisition_mode1 = Component(EpicsSignal, ':GETCS2.SCAN')
-    acquire = Component(EpicsSignal, ':GETCS',put_complete=True)  # Rely on the IOC to signal done-ness.
-    enabled = Component(EpicsSignal, ':ENABLE_IC_UPDATES')
+class I400(SingleTrigger, DetectorBase):
+    _default_read_attrs = None
+    _default_configuration_attrs = None
+
+    _status_type = DeviceStatus
+    gain = Component(Signal, ':RANGE_BP')
+    exposure_time = Component(Signal, ':PERIOD_SP',put_complete=True)
+    acquisition_mode = Component(Signal, ':GETCS.SCAN')
+    acquisition_mode1 = Component(Signal, ':GETCS2.SCAN')
+    acquire = Component(Signal, ':GETCS',put_complete=True)  # Rely on the IOC to signal done-ness.
+    enabled = Component(Signal, ':ENABLE_IC_UPDATES')
     exptime_save = .5
     gain_save = 7
     ignore_triggers = 0
@@ -54,62 +61,27 @@ class I400(Device):
         ))
 
     def stage(self):
-        if self._staged == Staged.yes:
-            self.ignore_triggers += 1
-            return []
         self.kind = 'hinted'
         output = [super().stage()]
         self.acquisition_mode.set(0)
         self.acquisition_mode1.set(0)
         self.exposure_time.set(self.exptime_save)
         self.gain.set(self.gain_save)
-        print(self.exptime_save)
         return output.append(self)
 
     def unstage(self):
-        self.ignore_triggers = 0
         self.kind = 'normal'
         self.acquisition_mode.set(7)
         self.acquisition_mode1.set(7)
         self.exposure_time.set(.4)
         return [self].append(super().unstage())
 
-    class I400Channel(EpicsSignal):
-        # readback = Component(EpicsSignalRO, '', kind='hinted')
 
-        # def read(self):
-        #     value = self.get().readback
-        #     return{self.name: {'value': value,'timestamp': time.time()}}
 
-        def trigger(self):
-            """
-            Trigger the detector and return a Status object.
-            """
-            self.kind = 'hinted'
-            st = self.parent.trigger()
-            return st
-
-        def set_exposure(self, exptime):
-            self.parent.set_exposure(exptime)
-
-        def stage(self):
-            self.parent.stage()
-            print('staging channel')
-            self.kind = 'hinted'
-            return [self]#.append(super().stage())
-
-        def unstage(self):
-            print('unstaging channel')
-            self.kind = 'normal'
-            #self.parent.acquisition_mode.set(7)
-            #self.parent.acquisition_mode1.set(7)
-            #self.parent.exposure_time.set(.4)
-            return [self].append(self.parent.unstage())
-
-    Channel_1 = Component(I400Channel, ':IC1_MON', kind='hinted')
-    Channel_2 = Component(I400Channel, ':IC2_MON', kind='hinted')
-    Channel_3 = Component(I400Channel, ':IC3_MON', kind='hinted')
-    Channel_4 = Component(I400Channel, ':IC4_MON', kind='hinted')
+    Channel_1 = Component(StatsPlugin, ':IC1_MON', kind='hinted')
+    Channel_2 = Component(StatsPlugin, ':IC2_MON', kind='hinted')
+    Channel_3 = Component(StatsPlugin, ':IC3_MON', kind='hinted')
+    Channel_4 = Component(StatsPlugin, ':IC4_MON', kind='hinted')
 
 
 RSoXS_Diodes = I400('XF:07ID-ES1{DMR:I400-1}', name='I400 diode')
