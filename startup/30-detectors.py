66583cf27467c1234b7bff737e12bfed95a614db
RSoXS <egann@bnl.gov>
2019-11-14 10:54:10 -0500

    energy timeouts added

--- startup/30-detectors.py
+++ startup/30-detectors.py
@@ -81,7 +81,7 @@ class RSOXSGreatEyesDetector(SingleTrigger, GreatEyesDetector):
         return [self].append(super().unstage(*args, **kwargs))
 
     def set_exptime(self,secs):
-        self.cam.acquire_time.set(secs)
+        self.cam.acquire_time.set(secs,wait=True)
 
     def exptime(self):
         return ("{} has an exposure time of {} seconds".format(
