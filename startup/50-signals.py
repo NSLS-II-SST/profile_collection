92711ecd909bf6e76f5e1439e20416ee61d7825e
RSoXS <egann@bnl.gov>
2019-11-29 10:35:37 -0500

    stagesigs didn't work

--- startup/50-signals.py
+++ startup/50-signals.py
@@ -12,7 +12,7 @@ Beamstop_WAXS = EpicsSignalRO('XF:07ID-ES1{DMR:I400-1}:IC1_MON',name = 'WAXS Bea
 Beamstop_SAXS = EpicsSignalRO('XF:07ID-ES1{DMR:I400-1}:IC2_MON',name = 'SAXS Beamstop', kind='normal')
 IzeroDiode    = EpicsSignalRO('XF:07ID-ES1{DMR:I400-1}:IC4_MON',name = 'Izero Photodiode', kind='normal')
 
-IzeroMeshI4    = EpicsSignalRO('XF:07ID-ES1{Slt1:I400-1}:IC4_MON',name = 'Izero Mesh I400', kind='normal')
+IzeroMesh    = EpicsSignalRO('XF:07ID-ES1{Slt1:I400-1}:IC4_MON',name = 'Izero Mesh I400', kind='normal')
 Sample_Drain = EpicsSignalRO('XF:07ID-ES1{Slt1:I400-1}:IC1_MON',name = 'RSoXS Drain', kind='normal')
 SlitBottom_I = EpicsSignalRO('XF:07ID-ES1{Slt1:I400-1}:IC2_MON',name = 'Slit Bottom', kind='normal')
 SlitTop_I    = EpicsSignalRO('XF:07ID-ES1{Slt1:I400-1}:IC3_MON',name = 'Slit Top', kind='normal')
