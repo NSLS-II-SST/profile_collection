7371c9d309aa2d48eda985b3263d178d6fb9edd3
RSoXS <egann@bnl.gov>
2020-02-16 18:50:08 -0500

    swap beamstop cable to DMR I400

--- startup/50-signals.py
+++ startup/50-signals.py
@@ -8,8 +8,8 @@ from ophyd import EpicsSignalRO, EpicsSignal
 bpm13_sum = EpicsSignalRO('XF:07ID-BI{BPM:13}Stats5:Total_RBV', name='Downstream Izero Phosphor Intensity')
 
 ring_current = EpicsSignalRO('SR:OPS-BI{DCCT:1}I:Real-I', name='NSLS-II Ring Current', kind='normal')
-Beamstop_WAXS = EpicsSignalRO('XF:07ID-ES1{Slt1:I400-1}:IC1_MON',name = 'WAXS Beamstop', kind='normal')
-Beamstop_SAXS = EpicsSignalRO('XF:07ID-ES1{DMR:I400-1}:IC2_MON',name = 'SAXS Beamstop', kind='normal')
+Beamstop_WAXS = EpicsSignalRO('XF:07ID-ES1{DMR:I400-1}:IC1_MON',name = 'WAXS Beamstop', kind='normal')
+Beamstop_SAXS = EpicsSignalRO('XF:07ID-ES1{Slt1:I400-1}:IC2_MON',name = 'SAXS Beamstop', kind='normal')
 Izero_Diode    = EpicsSignalRO('XF:07ID-ES1{DMR:I400-1}:IC3_MON',name = 'Izero Photodiode', kind='normal')
 
 #IzeroMesh    = EpicsSignalRO('XF:07ID-ES1{Slt1:I400-1}:IC4_MON',name = 'Izero Mesh I400', kind='normal')
@@ -29,7 +29,7 @@ Slit1_Top_I  = EpicsSignalRO('XF:07ID1-BI{EM:1}EM180:Current3:MeanValue_RBV',
 Slit1_IB_I   = EpicsSignalRO('XF:07ID1-BI{EM:1}EM180:Current4:MeanValue_RBV',
                            name = 'RSoXS Slit 1 Inbound Current', kind='normal')
 
-DiodeRange = EpicsSignal('XF:07ID-ES1{DMR:I400-1}:RANGE_BP')
+DiodeRange = EpicsSignal('XF:07ID-ES1{Slt1:I400-1}:RANGE_BP')
 
 DM7_Diode = EpicsSignalRO('XF:07ID-BI{DM7:I400-1}:IC4_MON',name = 'DM7 Photodiode', kind='normal')
 DM4_PD            = EpicsSignalRO('XF:07ID-BI{DM5:F4}Cur:I3-I', name='DM4 Photodiode', kind='normal')
