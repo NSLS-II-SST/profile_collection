24cc941176a9821b46d0b5a87ffaaa0730f2a246
Maksim Rakitin <mrakitin@bnl.gov>
2020-03-05 22:11:58 -0500

    Upgrade pyepics

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-conda install -y -c ${CONDA_CHANNEL_NAME} ansiwrap "ophyd>=1.4.0rc5" bluesky-darkframes
+conda install -y -c ${CONDA_CHANNEL_NAME} \
+    ansiwrap \
+    "ophyd>=1.4.0rc5" \
+    bluesky-darkframes \
+    "pyepics>=3.4.1"
