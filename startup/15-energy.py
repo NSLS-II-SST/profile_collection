1ead8c98a3a89ff039b2164a4f6c09999b52ff51
RSoXS <egann@bnl.gov>
2020-03-14 15:00:02 -0400

    new energy calculations

--- startup/15-energy.py
+++ startup/15-energy.py
@@ -303,8 +303,8 @@ class EnPos(PseudoPositioner):
     monoen = Cpt(Monochromator, 'XF:07ID1-OP{Mono:PGM1-Ax:',kind='hinted',name='Mono Energy')
     epugap = Cpt(UndulatorMotor, 'SR:C07-ID:G1A{SST1:1-Ax:Gap}-Mtr',kind='normal',name='EPU Gap')
     epuphase = Cpt(UndulatorMotor, 'SR:C07-ID:G1A{SST1:1-Ax:Phase}-Mtr',kind='normal',name='EPU Phase')
-    epumode = Cpt(PVPositioner,'SR:C07-ID:G1A{SST1:1-Ax:Phase}Phs:Mode-RB',
-                  write_pv='SR:C07-ID:G1A{SST1:1-Ax:Phase}Phs:Mode-SP',
+    epumode = Cpt(PVPositioner,'SR:C07-ID:G1A{SST1:1-Ax:Phase}Phs:Mode-SP',
+                  read_pv='SR:C07-ID:G1A{SST1:1-Ax:Phase}Phs:Mode-RB',
                   name='EPU Mode', kind='normal')
 
     @pseudo_position_argument
