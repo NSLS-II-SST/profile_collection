b8bffc7f9caa1ee40aa482b25fad14d32606c91a
Eliot Gann <37706296+EliotGann@users.noreply.github.com>
2020-06-18 10:54:06 -0400

    Adding in dummy detector

---
+++ startup/30-dummysaxs.py
@@ -0,0 +1,15 @@
+run_report(__file__)
+
+from ophyd.sim import SynSignalWithRegistry
+from ophyd import Device, Component, Signal
+
+def make_random_array():
+    # return numpy array
+    return np.zeros(1000,1000)
+
+class SimGreatEyes(Device):
+    image = Component(SynSignalWithRegistry, func=make_random_array, save_path='/tmp/sim_detector_stroage/')
+    some_other_thing = Component(Signal, value=3)
+    a_dynamic_thing = Component(SynSignal, func=lambda: 3 + random.random())
+
+saxs_det = SimGreateyes()
