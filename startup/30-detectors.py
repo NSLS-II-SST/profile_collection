69e9482efb3d04263f517b00edb5fbbc9ed47acc
Eliot Gann <37706296+EliotGann@users.noreply.github.com>
2020-06-18 10:39:00 -0400

    Taking out detectors so we can startup bluesky

--- startup/30-detectors.py
+++ startup/30-detectors.py
@@ -146,12 +146,17 @@ class RSOXSGreatEyesDetector(SingleTrigger, GreatEyesDetector):
     #needs to be run on the server
     # sudo mount -t cifs //10.7.0.217/data/ /mnt/zdrive -o user=linuxuser,pass=greateyes
 
+# turning detector to simulated start
+#saxs_det = RSOXSGreatEyesDetector('XF:07ID1-ES:1{GE:1}', name='Small Angle CCD Detector',
+#                                  read_attrs=['tiff', 'stats1.total'])
 
-saxs_det = RSOXSGreatEyesDetector('XF:07ID1-ES:1{GE:1}', name='Small Angle CCD Detector',
-                                  read_attrs=['tiff', 'stats1.total'])
+
+
+# turning detector to simulated stop
+
 #waxs_det = RSOXSGreatEyesDetector('XF:07ID1-ES:1{GE:2}', name='Wide Angle CCD Detector',
 #                                  read_attrs=['tiff', 'stats1.total'])
-saxs_det.transform_type = 3
+#####saxs_det.transform_type = 3
 #waxs_det.transform_type = 1
 
 
@@ -233,35 +238,38 @@ class SyncedDetectors(Device):
 #change this to saxs or waxs to record what the shutter state is
 #shutter_status_w = sw_det.waxs.cam.sync
 #shutter_status_s = saxs_det.cam.sync
-shutter_status_s = saxs_det.cam.shutter_mode
-shutter_status_s.name = 'shutter mode saxs'
+###shutter_status_s = saxs_det.cam.shutter_mode
+###shutter_status_s.name = 'shutter mode saxs'
 #sw_det.waxs.cam.acquire_time.name = 'WAXS Exposure'
 #sw_det.saxs.cam.acquire_time.name = 'SAXS Exposure'
 #sw_det.saxs.transform_type = 3
 #sw_det.waxs.transform_type = 1
 
-for det in [saxs_det]:#, waxs_det,sw_det.waxs,sw_det.saxs]:
-    det.kind = 'normal'
-    det.stats1.kind = 'hinted'
-    det.stats1.total.kind = 'hinted'
-    det.cam.kind = 'normal'
-    det.cam.temperature_actual.kind = 'normal'
-    det.cam.hot_side_temp.kind = 'normal'
-    det.cam.bin_y.kind = 'normal'
-    det.cam.bin_x.kind = 'normal'
-    det.cam.adc_speed.kind = 'normal'
-    det.cam.acquire_time.kind = 'normal'
-    det.cam.model.kind = 'normal'
-    det.cam.trigger_mode.kind = 'normal'
-    det.cam.sync.kind = 'normal'
-    det.cam.shutter_mode.kind = 'normal'
-    det.cam.shutter_open_delay.kind = 'normal'
-    det.cam.shutter_close_delay.kind = 'normal'
-    det.cam.min_x.kind = 'normal'
-    det.cam.temperature.kind = 'normal'
-    det.cam.min_y.kind = 'normal'
+# for det in [saxs_det]:#, waxs_det,sw_det.waxs,sw_det.saxs]:
+#     det.kind = 'normal'
+#     det.stats1.kind = 'hinted'
+#     det.stats1.total.kind = 'hinted'
+#     det.cam.kind = 'normal'
+#     det.cam.temperature_actual.kind = 'normal'
+#     det.cam.hot_side_temp.kind = 'normal'
+#     det.cam.bin_y.kind = 'normal'
+#     det.cam.bin_x.kind = 'normal'
+#     det.cam.adc_speed.kind = 'normal'
+#     det.cam.acquire_time.kind = 'normal'
+#     det.cam.model.kind = 'normal'
+#     det.cam.trigger_mode.kind = 'normal'
+#     det.cam.sync.kind = 'normal'
+#     det.cam.shutter_mode.kind = 'normal'
+#     det.cam.shutter_open_delay.kind = 'normal'
+#     det.cam.shutter_close_delay.kind = 'normal'
+#     det.cam.min_x.kind = 'normal'
+#     det.cam.temperature.kind = 'normal'
+#     det.cam.min_y.kind = 'normal'
+
+
+
 #sw_det.kind = 'hinted'
 
 #sw_det.read_attrs = ['saxs','waxs']
 
-sd.baseline.extend([saxs_det.cam])#, waxs_det.cam])
+#sd.baseline.extend([saxs_det.cam])#, waxs_det.cam])
