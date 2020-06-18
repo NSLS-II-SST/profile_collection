bdf9ac166c635d9ee3d94047bdd27883c416dcf2
Eliot Gann <37706296+EliotGann@users.noreply.github.com>
2020-06-18 11:12:46 -0400

    Adding in dummy detector

--- startup/30-dummysaxs.py
+++ startup/30-dummysaxs.py
@@ -7,12 +7,14 @@ import numpy as np
 def make_random_array():
     # return numpy array
     return np.zeros([1000,1000])
+class SimGreatEyesCam(Device):
+    shutter_mode = Component(Signal, value=3)
+    acquire_time = Component(SynSignal, func=lambda: 3 + np.random.rand())
+
 
 class SimGreatEyes(Device):
     image = Component(SynSignalWithRegistry, func=make_random_array, save_path='/tmp/sim_detector_stroage/')
-    cam= Component(Device)
-    cam.shutter_mode= Component(Signal, value=3)
+    cam= Component(SimGreatEyesCam)
 
-    cam.acquire_time, = Component(SynSignal, func=lambda: 3 + np.random.rand())
 
 saxs_det = SimGreatEyes(name="Simulated SAXS camera")
