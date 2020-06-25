b86506da0bee2d102ecb57090c23b44e3d569319
Eliot Gann <37706296+EliotGann@users.noreply.github.com>
2020-06-25 16:28:18 -0400

    Adding in dummy detector

--- startup/30-dummysaxs.py
+++ startup/30-dummysaxs.py
@@ -47,7 +47,7 @@ class SimGreatEyes(Device):
 
     def trigger(self):
         print("trigger")
-        yield from self.image.trigger()
+        return self.image.trigger()
 
     def collect_asset_docs(self):
         print('collecting documents')
