ba9f1f5ab420a42866fd7b24ecd5a582e5aa55e4
Eliot Gann <37706296+EliotGann@users.noreply.github.com>
2020-06-25 15:04:23 -0400

    Adding in dummy detector

--- startup/30-dummysaxs.py
+++ startup/30-dummysaxs.py
@@ -29,7 +29,7 @@ class SimGreatEyes(Device):
     cam= Component(SimGreatEyesCam)
 
     def collect_asset_docs(self):
-        yield from self.image.collect_asset_docs()
+        self.image.collect_asset_docs()
     def shutter(self):
         switch = {
             0: 'disabled',
