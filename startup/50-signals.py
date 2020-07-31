6ae5756e6460df803ec9a427a23f62fdaf0a0336
RSoXS <egann@bnl.gov>
2020-07-31 18:26:11 -0400

    moved beamstop

--- startup/50-signals.py
+++ startup/50-signals.py
@@ -9,7 +9,7 @@ bpm13_sum = EpicsSignalRO('XF:07ID-BI{BPM:13}Stats5:Total_RBV', name='Downstream
 
 ring_current = EpicsSignalRO('SR:OPS-BI{DCCT:1}I:Real-I', name='NSLS-II Ring Current', kind='normal')
 Beamstop_WAXS = EpicsSignalRO('XF:07ID-ES1{DMR:I400-1}:IC1_MON',name = 'WAXS Beamstop', kind='normal')
-Beamstop_SAXS = EpicsSignalRO('XF:07ID-ES1{Slt1:I400-1}:IC2_MON',name = 'SAXS Beamstop', kind='normal')
+Beamstop_SAXS = EpicsSignalRO('XF:07ID-ES1{DMR:I400-1}:IC2_MON',name = 'SAXS Beamstop', kind='normal')
 Izero_Diode    = EpicsSignalRO('XF:07ID-ES1{DMR:I400-1}:IC3_MON',name = 'Izero Photodiode', kind='normal')
 
 #IzeroMesh    = EpicsSignalRO('XF:07ID-ES1{Slt1:I400-1}:IC4_MON',name = 'Izero Mesh I400', kind='normal')
