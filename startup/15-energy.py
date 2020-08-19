25af819b2370a2880c821eafb7f309312c114a08
RSoXS <egann@bnl.gov>
2020-08-19 12:07:59 -0400

    mono adding in components again

--- startup/15-energy.py
+++ startup/15-energy.py
@@ -27,6 +27,8 @@ class Monochromator(PVPositioner):
     mirror2 = Cpt(prettymotor, 'MirP}Mtr', name="Mono Mirror", kind='normal')
     cff = Cpt(EpicsSignal, ':CFF_SP', name="Mono CFF", kind='normal')
     vls = Cpt(EpicsSignal, ':VLS_B2.A', name="Mono CFF", kind='normal')
+    gratingtype = Cpt(EpicsSignal, 'GrtX}Mtr_TYPE_SP', readback='GrtX}Mtr_TYPE_MON',name="Mono Grating Type", kind='normal')
+    mirror2type = Cpt(EpicsSignal, 'MirX}Mtr_TYPE_SP', readback='MirX}Mtr_TYPE_MON',name="Mono Mirror Type", kind='normal')
 
     done = Cpt(EpicsSignalRO, ':ERDY_STS')
     done_value = 1
@@ -430,7 +432,7 @@ mono_en = en.monoen
 epu_gap = en.epugap
 epu_phase = en.epuphase
 #epu_mode = en.epumode
-mono_en.read_attrs = ['readback']
+#mono_en.read_attrs = ['readback']
 en.epugap.kind = 'normal'
 # en.read_attrs = ['energy',
 #                  'energy.readback',
