afcbda4031ad5db41c576958bfc6698c8d0c48b4
Joshua Lynch <jlynch@bnl.gov>
2020-08-28 10:00:18 -0400

    set RE.md after configure_base

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -4,6 +4,9 @@ import os
 
 import nslsii
 
+from pathlib import Path
+
+import appdirs
 
 # Use caproto not pyepics.
 os.environ['OPHYD_CONTROL_LAYER'] = 'caproto'
@@ -63,6 +66,49 @@ nslsii.configure_base(
 #	movr : bluesky.plan_stubs.movr
 #	bpp : bluesky.preprocessors
 
+
+try:
+    from bluesky.utils import PersistentDict
+except ImportError:
+    import msgpack
+    import msgpack_numpy
+    import zict
+
+    class PersistentDict(zict.Func):
+        def __init__(self, directory):
+            self._directory = directory
+            self._file = zict.File(directory)
+            super().__init__(self._dump, self._load, self._file)
+
+        @property
+        def directory(self):
+            return self._directory
+
+        def __repr__(self):
+            return f"<{self.__class__.__name__} {dict(self)!r}>"
+
+        @staticmethod
+        def _dump(obj):
+            "Encode as msgpack using numpy-aware encoder."
+            # See https://github.com/msgpack/msgpack-python#string-and-binary-type
+            # for more on use_bin_type.
+            return msgpack.packb(
+                obj,
+                default=msgpack_numpy.encode,
+                use_bin_type=True)
+
+        @staticmethod
+        def _load(file):
+            return msgpack.unpackb(
+                file,
+                object_hook=msgpack_numpy.decode,
+                raw=False)
+
+runengine_metadata_dir = appdirs.user_data_dir(appname="bluesky") / Path("runengine-metadata")
+
+# PersistentDict will create the directory if it does not exist
+RE.md = PersistentDict(runengine_metadata_dir)
+
 # Optional: set any metadata that rarely changes.
 RE.md['beamline_id'] = 'SST-1 RSoXS'
 
@@ -85,3 +131,6 @@ bec.disable_baseline()
 from bluesky.callbacks.zmq import Publisher
 publisher = Publisher('localhost:5577')
 RE.subscribe(publisher)
+
+logging.getLogger('ophyd').setLevel('WARNING')
+
