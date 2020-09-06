b6b7d868426d2cecd2387c6ede5e50a17c587cbd
RSoXS <egann@bnl.gov>
2020-09-06 11:24:04 -0400

    working on eliot_scan

--- startup/94-NEXAFSplans.py
+++ startup/94-NEXAFSplans.py
@@ -180,6 +180,37 @@ def full_Fluorine_NEXAFS(sigs=[],
                                 openshutter=True,diode_range=diode_range,m3_pitch=m3_pitch, pol=pol,
                                 open_each_step=open_each_step,exp_time=exp_time,grating=grating)
 
+def short_Fluorine_NEXAFS(sigs=[],
+                        dets=[Sample_TEY,Izero_Mesh,Beamstop_WAXS], energy=en,pol=100,m3_pitch=7.90,diode_range=7,
+                       open_each_step=True,exp_time=1,grating='no change'):
+    '''
+    Full Nitrogen Scan runs an RSoXS sample set through the carbon edge, with particular emphasis in he pre edge region
+
+
+    :param sigs: which other signals to use
+    :param dets: which detector to use
+    :param energy: what energy motor to scan
+    :return: perform scan
+
+    normal scan takes ~ 7 minutes to complete
+    '''
+    enscan_type = 'full_Fluorine_NEXAFS'
+    sample()
+    if len(read_input("Starting a Nitrogen NEXAFS scan hit enter in the next 3 seconds to abort"
+                      "\nYou remembered to hook up the shutter, right?", "abort", "", 3)) > 0:
+        return
+    # create a list of energies
+    energies = np.arange(680,720,.5)
+    times = energies.copy()
+
+    # Define exposures times for different energy ranges
+    # use these energies and exposure times to scan energy and record detectors and signals
+
+
+    yield from NEXAFS_scan_core(sigs, dets, energy, energies,enscan_type=enscan_type,
+                                openshutter=True,diode_range=diode_range,m3_pitch=m3_pitch, pol=pol,
+                                open_each_step=open_each_step,exp_time=exp_time,grating=grating)
+
 
 def full_Oxygen_NEXAFS(sigs=[],
                         dets=[Sample_TEY,Izero_Mesh,Beamstop_WAXS], energy=en,pol=100,m3_pitch=7.90,diode_range=6,
@@ -203,6 +234,40 @@ def full_Oxygen_NEXAFS(sigs=[],
     # create a list of energies
     energies = np.arange(510,525,1)
     energies = np.append(energies,np.arange(525,540,.2))
+    energies = np.append(energies,np.arange(540,560,1))
+    times = energies.copy()
+
+    # Define exposures times for different energy ranges
+    # use these energies and exposure times to scan energy and record detectors and signals
+
+
+    yield from NEXAFS_scan_core(sigs, dets, energy, energies,enscan_type=enscan_type,
+                                openshutter=True,diode_range=diode_range,m3_pitch=m3_pitch, pol=pol,
+                                open_each_step=open_each_step,exp_time=exp_time,grating=grating)
+
+
+def short_Oxygen_NEXAFS(sigs=[],
+                        dets=[Sample_TEY,Izero_Mesh,Beamstop_WAXS], energy=en,pol=100,m3_pitch=7.90,diode_range=6,
+                       open_each_step=True,exp_time=1,grating='no change'):
+    '''
+    Full Carbon Scan runs an RSoXS sample set through the carbon edge, with particular emphasis in he pre edge region
+
+
+    :param sigs: which other signals to use
+    :param dets: which detector to use
+    :param energy: what energy motor to scan
+    :return: perform scan
+
+    normal scan takes ~ 7 minutes to complete
+    '''
+    enscan_type = 'full_Oxygen_NEXAFS'
+    sample()
+    if len(read_input("Starting a Oxygen NEXAFS scan hit enter in the next 3 seconds to abort"
+                      "\nYou remembered to hook up the shutter, right?", "abort", "", 3)) > 0:
+        return
+    # create a list of energies
+    energies = np.arange(510,525,1)
+    energies = np.append(energies,np.arange(525,540,.4))
     energies = np.append(energies,np.arange(540,560,1))
     times = energies.copy()
 
