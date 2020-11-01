af30a82183eae02190ce4dee403639fe1b5d22ba
Eliot Gann <37706296+EliotGann@users.noreply.github.com>
2020-11-01 10:50:32 -0500

    adding in skinny staging and unstaging for darkplan

--- startup/30-detectors.py
+++ startup/30-detectors.py
@@ -67,7 +67,7 @@ class RSOXSGreatEyesDetector(SingleTrigger, GreatEyesDetector):
 
 
     def skinnystage(self, *args, **kwargs):
-        yield from [self].append(super().stage(*args, **kwargs))
+        return (yield Msg('stage',super()))
 
     def shutter(self):
         switch = {
@@ -93,7 +93,7 @@ class RSOXSGreatEyesDetector(SingleTrigger, GreatEyesDetector):
         return [self].append(super().unstage(*args, **kwargs))
 
     def skinnyunstage(self, *args, **kwargs):
-        yield from [self].append(super().unstage(*args, **kwargs))
+        return (yield Msg('unstage',super()))
 
     def set_exptime(self,secs):
         self.cam.acquire_time.set(secs)
