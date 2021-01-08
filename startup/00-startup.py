c542f0d4c79abce24f1109eac69a8b903f19077a
Michael Hart <mhart@bnl.gov>
2021-01-08 15:58:50 -0500

    Updates for 2021-1.0 deployment

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -9,7 +9,7 @@ from pathlib import Path
 import appdirs
 
 # Use caproto not pyepics.
-os.environ['OPHYD_CONTROL_LAYER'] = 'caproto'
+os.environ['OPHYD_CONTROL_LAYER'] = 'pyepics'
 
 
 def colored(text, tint='white', attrs=[]):
@@ -67,6 +67,7 @@ nslsii.configure_base(
 #	bpp : bluesky.preprocessors
 
 
+# === START PERSISTENT DICT CODE ===
 
 try:
     from bluesky.utils import PersistentDict
@@ -76,14 +77,48 @@ except ImportError:
     import zict
 
     class PersistentDict(zict.Func):
+        """
+        A MutableMapping which syncs it contents to disk.
+        The contents are stored as msgpack-serialized files, with one file per item
+        in the mapping.
+        Note that when an item is *mutated* it is not immediately synced:
+        >>> d['sample'] = {"color": "red"}  # immediately synced
+        >>> d['sample']['shape'] = 'bar'  # not immediately synced
+        but that the full contents are synced to disk when the PersistentDict
+        instance is garbage collected.
+        """
         def __init__(self, directory):
             self._directory = directory
             self._file = zict.File(directory)
+            self._cache = {}
             super().__init__(self._dump, self._load, self._file)
+            self.reload()
+
+            # Similar to flush() or _do_update(), but without reference to self
+            # to avoid circular reference preventing collection.
+            # NOTE: This still doesn't guarantee call on delete or gc.collect()!
+            #       Explicitly call flush() if immediate write to disk required.
+            def finalize(zfile, cache, dump):
+                zfile.update((k, dump(v)) for k, v in cache.items())
+
+            import weakref
+            self._finalizer = weakref.finalize(
+                self, finalize, self._file, self._cache, PersistentDict._dump)
 
         @property
         def directory(self):
             return self._directory
+
+        def __setitem__(self, key, value):
+            self._cache[key] = value
+            super().__setitem__(key, value)
+
+        def __getitem__(self, key):
+            return self._cache[key]
+
+        def __delitem__(self, key):
+            del self._cache[key]
+            super().__delitem__(key)
 
         def __repr__(self):
             return f"<{self.__class__.__name__} {dict(self)!r}>"
@@ -105,10 +140,22 @@ except ImportError:
                 object_hook=msgpack_numpy.decode,
                 raw=False)
 
+        def flush(self):
+            """Force a write of the current state to disk"""
+            for k, v in self.items():
+                super().__setitem__(k, v)
+
+        def reload(self):
+            """Force a reload from disk, overwriting current cache"""
+            self._cache = dict(super().items())
+
 runengine_metadata_dir = appdirs.user_data_dir(appname="bluesky") / Path("runengine-metadata")
 
 # PersistentDict will create the directory if it does not exist
 RE.md = PersistentDict(runengine_metadata_dir)
+
+# === END PERSISTENT DICT CODE ===
+
 
 # Optional: set any metadata that rarely changes.
 RE.md['beamline_id'] = 'SST-1 RSoXS'
@@ -133,8 +180,6 @@ from bluesky.callbacks.zmq import Publisher
 publisher = Publisher('localhost:5577')
 RE.subscribe(publisher)
 
-logging.getLogger('ophyd').setLevel('WARNING')
-
 import logging
 import bluesky.log
 logger = logging.getLogger('bluesky_darkframes')
@@ -146,3 +191,4 @@ logger.setLevel('DEBUG')  # change DEBUG to INFO later on
 
 from databroker.v0 import Broker
 db0 = Broker.named('rsoxs')
+
