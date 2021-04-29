f6b530ce61ea333293ec90bc55a7f88282f7e731
Maksim Rakitin <mrakitin@bnl.gov>
2021-04-28 22:32:40 -0400

    CI: generate metadata based on RE.md in startup/90b-samples_users.py

--- .ci/gen-metadata.py
+++ .ci/gen-metadata.py
@@ -7,22 +7,28 @@ md_path = os.environ.get("RE_METADATA")
 if not md_path:
     raise RuntimeError("Env. variable 'RE_METADATA' is not defined or empty.")
 
+# Get all keys based on the RE.md occurrences in the module.
+md_keys = []
+with open("startup/90b-samples_users.py") as f:
+    lines = f.readlines()
+
+    for line in lines:
+        if "=RE.md" in line or "= RE.md" in line:
+            # Parsing lines such as:
+            # ...
+            # sample_state = RE.md['sample_state']
+            # ...
+            # proposal_id=RE.md['proposal_id'],
+            # ...
+            key = line.split("=")[-1].strip().split("'")[1]
+            md_keys.append(key)
+    md_keys.sort()
+
 md = PersistentDict(md_path)
 
-keys = [
-    "institution",
-    "project_desc",
-    "project_name",
-    "proposal_id",
-    "saf_id",
-    "user_id",
-    "user_name",
-    "user_start_date",
-]
-
-for k in keys:
+# Add the keys to the metadata dict with fake values:
+for k in md_keys:
     md[k] = f"test_{k}"
-md["user_email"] = ""
 
 print(f"Metadata dir: {md.directory}")
 print(f"Repr: {md.__repr__()}")
