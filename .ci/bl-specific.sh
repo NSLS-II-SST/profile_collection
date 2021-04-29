d6e6a674f13d17cafd455a432b6ff8210e95d87f
Maksim Rakitin <mrakitin@bnl.gov>
2021-04-28 21:15:27 -0400

    CI: add fake metadata

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -3,3 +3,4 @@
 sudo mkdir -v -p /areadata/rsoxs-collection/runengine-metadata
 sudo chown -Rv $USER: /areadata/rsoxs-collection/runengine-metadata
 
+cp -v .ci/fake-metadata/* /areadata/rsoxs-collection/runengine-metadata/
