16ee89029011ae3dae6b81432676d83162281eb2
Eliot Gann <37706296+EliotGann@users.noreply.github.com>
2021-05-25 14:34:03 -0400

    change mirrors to fmb style from csx

--- startup/09-mirrors.py
+++ startup/09-mirrors.py
@@ -19,9 +19,33 @@ class HexapodMirror(Device):
 #        self.Yaw.read()
 
 
+class FMBHexapodMirrorAxis(PVPositioner):
+    readback = Cpt(EpicsSignalRO, 'Mtr_MON')
+    setpoint = Cpt(EpicsSignal, 'Mtr_POS_SP')
+    actuate = FmtCpt(EpicsSignal, '{self.parent.prefix}}}MOVE_CMD.PROC')
+    actual_value = 1
+    stop_signal = FmtCpt(EpicsSignal, '{self.parent.prefix}}}STOP_CMD.PROC')
+    stop_value = 1
+    done = FmtCpt(EpicsSignalRO, '{self.parent.prefix}}}BUSY_STS')
+    done_value = 0
+
+
+class FMBHexapodMirror(Device):
+    z = Cpt(FMBHexapodMirrorAxis, '-Ax:Z}')
+    y = Cpt(FMBHexapodMirrorAxis, '-Ax:Y}')
+    x = Cpt(FMBHexapodMirrorAxis, '-Ax:X}')
+    pit = Cpt(FMBHexapodMirrorAxis, '-Ax:P}')
+    yaw = Cpt(FMBHexapodMirrorAxis, '-Ax:Yaw}')
+    rol = Cpt(FMBHexapodMirrorAxis, '-Ax:R}')
+
+
 
 mir4 = HexapodMirror('XF:07ID2-OP{Mir:M4CD-Ax:',name='SST 1 Mirror 4',kind='hinted')
 mir3 = HexapodMirror('XF:07ID1-OP{Mir:M3ABC-Ax:',name='SST 1 Mirror 3',kind='hinted')
 mir1 = HexapodMirror('XF:07IDA-OP{Mir:M1-Ax:',name='SST 1 Mirror 1',kind='hinted')
+
+mir4f = FMBHexapodMirror('XF:07ID2-OP{Mir:M4CD',name='SST 1 Mirror 4 fmb',kind='hinted')
+mir3f = FMBHexapodMirror('XF:07ID1-OP{Mir:M3ABC',name='SST 1 Mirror 3 fmb',kind='hinted')
+mir1f = FMBHexapodMirror('XF:07IDA-OP{Mir:M1',name='SST 1 Mirror 1 fmb',kind='hinted')
 
 sd.baseline.extend([mir3,mir4,mir2_type])
