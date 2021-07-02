e535ab94c350bf21562d36f85dfca40b3f361f29
Eliot Gann <37706296+EliotGann@users.noreply.github.com>
2021-07-02 13:51:24 -0400

    deepcopy no mare

--- startup/91b-energyscans.py
+++ startup/91b-energyscans.py
@@ -5,16 +5,17 @@ import bluesky.plans as bp
 import bluesky.plan_stubs as bps
 from copy import deepcopy
 
-def clean_up_md(arguments={},md={}):
+def clean_up_md(arguments={},md={},**kwargs):
     del arguments['md']  # no recursion here!
-    arguments = deepcopy(arguments)
-    if 'kwargs' in arguments.keys():
-        if 'dets' in arguments['kwargs'].keys():
-            arguments['kwargs']['dets'] = [det.name for det in arguments['kwargs']['dets']]
-        if 'signals' in arguments['kwargs'].keys():
-            arguments['kwargs']['signals'] = [signal.name for signal in arguments['kwargs']['signals']]
-        if 'energy' in arguments['kwargs'].keys():
-            arguments['kwargs']['energy'] = arguments['kwargs']['energy'].name
+    del arguments['kwargs']
+    for key in kwargs:
+        if type(kwargs[key])==list: # dets, signals
+            arguments[key] = [object.name for object in kwargs[key]]
+        elif key is 'energy':
+            arguments[key] = kwargs[key].name
+        else:
+            arguments[key] = kwargs[key]
+
     md.get('plan_history', []).append({'plan_name': arguments['plan_name'],
                                        'arguments': arguments})
     md.update({'plan_name': arguments['enscan_type'], 'master_plan': arguments['master_plan']})
@@ -37,7 +38,7 @@ def full_oxygen_scan_nd(multiple=1, diode_range=6, m3_pitch=7.99, grating='1200'
     plan_name = 'full_oxygen_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     
     if len(read_input("Starting a Oxygen energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
@@ -75,7 +76,7 @@ def short_oxygen_scan_nd(multiple=1, diode_range=6, m3_pitch=7.98, grating='1200
     plan_name = 'short_oxygen_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Oxygen energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
     # create a list of energies
@@ -113,7 +114,7 @@ def short_zincl_scan_nd(multiple=1, diode_range=6, m3_pitch=7.98, grating='1200'
     plan_name = 'short_zincl_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Zinc energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
     # create a list of energies
@@ -147,7 +148,7 @@ def very_short_oxygen_scan_nd(multiple=1, diode_range=6, m3_pitch=7.97, grating=
     plan_name = 'very_short_oxygen_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Oxygen energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
     # create a list of energies
@@ -186,7 +187,7 @@ def short_fluorine_scan_nd(multiple=1, diode_range=7, m3_pitch=7.98, grating='12
     plan_name = 'short_fluorine_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a fluorine energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
 
@@ -225,7 +226,7 @@ def full_nitrogen_scan_nd(multiple=1, diode_range=6, m3_pitch=7.93, grating='120
     plan_name = 'full_nitrogen_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Nitrogen energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
     # create a list of energies
@@ -262,7 +263,7 @@ def short_nitrogen_scan_nd(multiple=1, diode_range=6, m3_pitch=8.00, grating='12
     plan_name = 'short_nitrogen_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Short Nitrogen energy scan "
                       "hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
@@ -302,7 +303,7 @@ def very_short_carbon_scan_nd(multiple=1, diode_range=6, m3_pitch=8.01, grating=
     plan_name = 'very_short_carbon_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a very short Carbon energy scan hit "
                       "enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
@@ -344,7 +345,7 @@ def short_carbon_scan_nd(multiple=1, diode_range=6, m3_pitch=8.00, grating='1200
     plan_name = 'short_carbon_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a short Carbon energy scan hit enter in "
                       "the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
@@ -386,7 +387,7 @@ def short_carbon_scan_nonaromatic(multiple=1, diode_range=6, m3_pitch=8.00, grat
     plan_name = 'short_carbon_scan_nonaromatic'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a short Carbon energy scan hit enter in "
                       "the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
@@ -429,7 +430,7 @@ def custom_rsoxs_scan(energies=[((270, 340, 1), 2)], master_plan=None,
     plan_name = 'custom_rsoxs_scan'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     newenergies = []
     newtimes = []
     if len(read_input("Starting a specified energy scan hit enter in "
@@ -464,7 +465,7 @@ def short_sulfurl_scan_nd(multiple=1, diode_range=6, m3_pitch=8.02, grating='120
     plan_name = 'short_sulfurl_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a short Sulfur L energy scan hit enter in "
                       "the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
@@ -504,7 +505,7 @@ def focused_carbon_scan_nd(multiple=1, diode_range=6, m3_pitch=7.93, grating='12
     plan_name = 'focused_carbon_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a short Carbon energy scan hit enter in "
                       "the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
@@ -546,7 +547,7 @@ def g_carbon_scan_nd(multiple=1, diode_range=6, m3_pitch=7.93, grating='1200', m
     plan_name = 'g_carbon_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Carbon energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
     # create a list of energies
@@ -579,7 +580,7 @@ def t_carbon_scan_nd(multiple=1, diode_range=6, m3_pitch=7.93, grating='1200', m
     plan_name = 't_carbon_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Carbon energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
     # create a list of energies
@@ -612,7 +613,7 @@ def sufficient_carbon_scan_nd(multiple=1, diode_range=6, m3_pitch=7.96, grating=
     plan_name = 'sufficient_carbon_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Carbon energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
     # create a list of energies
@@ -652,7 +653,7 @@ def picky_carbon_scan_nd(multiple=1, diode_range=6, m3_pitch=7.93, grating='1200
     plan_name = 'picky_carbon_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Carbon energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
     yield from bps.abs_set(mir3.Pitch, 7.96, wait=True)
@@ -686,7 +687,7 @@ def full_carbon_scan_nd(multiple=1, diode_range=6, m3_pitch=8.00, grating='1200'
     plan_name = 'full_carbon_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Carbon energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
     # create a list of energies
@@ -726,7 +727,7 @@ def full_carbon_scan_nonaromatic(multiple=1, diode_range=6, m3_pitch=7.97, grati
     plan_name = 'full_carbon_scan_nonaromatic'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Carbon energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
     # create a list of energies
@@ -766,7 +767,7 @@ def full_fluorine_scan_nd(multiple=1, diode_range=7, m3_pitch=7.89, grating='120
     plan_name = 'full_fluorine_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Fluorine energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
     # create a list of energies
@@ -801,7 +802,7 @@ def veryshort_fluorine_scan_nd(multiple=1, diode_range=7, m3_pitch=7.99, grating
     plan_name = 'veryshort_fluorine_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Fluorine energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
     # create a list of energies
@@ -837,7 +838,7 @@ def full_ca_scan_nd(multiple=1, diode_range=6, m3_pitch=8.00, grating='1200', ma
     plan_name = 'full_ca_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Calcium energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
     # create a list of energies
@@ -873,7 +874,7 @@ def short_calcium_scan_nd(multiple=1, diode_range=6, m3_pitch=8.00, grating='120
     plan_name = 'short_calcium_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Calcium energy scan hit enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
     # create a list of energies
@@ -908,7 +909,7 @@ def full_carbon_calcium_scan_nd(multiple=1, diode_range=6, m3_pitch=7.96, gratin
     plan_name = 'full_carbon_calcium_scan_nd'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a full carbon and calcium energy scan hit "
                       "enter in the next 3 seconds to abort", "abort", "", 3)) > 0:
         return
@@ -953,7 +954,7 @@ def survey_scan_verylowenergy(multiple=1, diode_range=7, m3_pitch=7.93, grating=
     plan_name = 'survey_scan_verylowenergy'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a low energy survey scan hit enter in the next 3 seconds to abort", "abort", "",
                       3)) > 0:
         return
@@ -987,7 +988,7 @@ def survey_scan_lowenergy(multiple=1, diode_range=7, m3_pitch=7.91, grating='120
     plan_name = 'survey_scan_lowenergy'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Low energy survey scan hit enter in the next 3 seconds to abort", "abort", "",
                       3)) > 0:
         return
@@ -1023,7 +1024,7 @@ def survey_scan_highenergy(multiple=1, diode_range=7, m3_pitch=7.89, grating='12
     plan_name = 'survey_scan_highenergy'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a High energy survey scan hit enter in the next 3 seconds to abort", "abort", "",
                       3)) > 0:
         return
@@ -1057,7 +1058,7 @@ def survey_scan_veryhighenergy(multiple=1, diode_range=7, m3_pitch=7.89, grating
     plan_name = 'survey_scan_veryhighenergy'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     if len(read_input("Starting a Very High energy survey scan hit enter in the next 3 seconds to abort", "abort", "",
                       3)) > 0:
         return
@@ -1094,7 +1095,7 @@ def cdsaxs_scan(energies=[(250, 2), (270, 2), (280, 2), (285, 2), (300, 2)],
     plan_name = 'cdsaxs_scan'
     # grab locals
     arguments = dict(locals())
-    clean_up_md(arguments,md)
+    clean_up_md(arguments,md,**kwargs)
     newenergies = []
     newtimes = []
     if len(read_input("Starting a CD-SAXS energy,angle scan hit enter in "
