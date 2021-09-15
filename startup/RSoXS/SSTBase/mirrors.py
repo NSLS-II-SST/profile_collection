896083de4278d534e46044048dcc6fdf89ba5f14
Eliot Gann <37706296+EliotGann@users.noreply.github.com>
2021-09-15 14:48:01 -0400

    further packagifying

--- startup/RSoXS/SSTBase/mirrors.py
+++ startup/RSoXS/SSTBase/mirrors.py
@@ -1,4 +1,4 @@
-from ophyd import (EpicsMotor, Device, Component as Cpt, EpicsSignal)
+from ophyd import (EpicsMotor, Device, Component as Cpt, EpicsSignal,PVPositioner)
 from ophyd import FormattedComponent as FmtCpt
 
 class HexapodMirror(Device):
