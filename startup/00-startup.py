387ad0e9e4b99fdd5f85887b9cd9b864ddaf07c3
Eliot Gann <37706296+EliotGann@users.noreply.github.com>
2021-10-02 14:04:38 -0400

    queueserver updates

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -37,7 +37,7 @@ from RSoXS.RSoXSBase.energyscans import *
 from RSoXS.RSoXSBase.NEXAFSscans import *
 from RSoXS.RSoXSObjects.slackbot import rsoxs_bot
 from RSoXS.RSoXSBase.acquisitions import *
-from .RSoXS.RSoXSBase.sample_spreadsheets import *
+from RSoXS.RSoXSBase.sample_spreadsheets import *
 
 
 try:
