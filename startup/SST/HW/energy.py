af944360a152db9d5949404d11a474f908580320
Eliot Gann <37706296+EliotGann@users.noreply.github.com>
2021-11-24 15:07:47 -0500

    gap calculation revamped from EPU measurements

--- startup/SST/HW/energy.py
+++ startup/SST/HW/energy.py
@@ -310,20 +310,22 @@ class EnPos(PseudoPositioner):
         **kwargs,
     ):
         super().__init__(a, **kwargs)
-        self.C250_gap = xr.load_dataarray(configpath / "EPU_C_250_gap.nc")
-        self.C250_intens = xr.load_dataarray(configpath / "EPU_C_250_intens.nc")
-        self.C1200_gap = xr.load_dataarray(configpath / "EPU_C_1200_gap.nc")
-        self.C1200_intens = xr.load_dataarray(configpath / "EPU_C_1200_intens.nc")
-        self.L250_gap = xr.load_dataarray(configpath / "EPU_L_250_gap.nc")
-        self.L250_intens = xr.load_dataarray(configpath / "EPU_L_250_intens.nc")
-        self.L1200_gap = xr.load_dataarray(configpath / "EPU_L_1200_gap.nc")
-        self.L1200_intens = xr.load_dataarray(configpath / "EPU_L_1200_intens.nc")
-        self.L250_gap_mrg = xr.load_dataarray(configpath / "EPU_L_250_gap_mrg.nc")
-        self.L250_intens_mrg = xr.load_dataarray(configpath / "EPU_L_250_intens_mrg.nc")
-        self.L1200_gap_mrg = xr.load_dataarray(configpath / "EPU_L_1200_gap_mrg.nc")
-        self.L1200_intens_mrg = xr.load_dataarray(
-            configpath / "EPU_L_1200_intens_mrg.nc"
-        )
+        self.gap_fit = np.zeros((10, 10))
+        self.gap_fit[0][:] = [889.981, 222.966, -0.945368, 0.00290731, -5.87973e-06, 7.80556e-09, -6.69661e-12,
+                              3.56679e-15, -1.07195e-18, 1.39775e-22]
+        self.gap_fit[1][:] = [-51.6545, -1.60757, 0.00914746, -2.65003e-05, 4.46303e-08, -4.8934e-11, 3.51531e-14,
+                              -1.4802e-17, 2.70647e-21, 0]
+        self.gap_fit[2][:] = [9.74128, 0.0528884, -0.000270428, 6.71135e-07, -6.68204e-10, 2.71974e-13, -2.82766e-17,
+                              -3.77566e-21, 0, 0]
+        self.gap_fit[3][:] = [-2.94165, -0.00110173, 3.13309e-06, -1.21787e-08, 1.21638e-11, -4.27216e-15, 3.59552e-19,
+                              0, 0, 0]
+        self.gap_fit[4][:] = [0.19242, 2.19545e-05, 6.11159e-08, 4.21707e-11, -6.84942e-14, 1.84302e-17, 0, 0, 0, 0]
+        self.gap_fit[5][:] = [-0.00615458, -9.55015e-07, -1.28929e-09, 4.28363e-13, 3.26302e-17, 0, 0, 0, 0, 0]
+        self.gap_fit[6][:] = [0.000113341, 1.90112e-08, 6.92088e-12, -1.87659e-15, 0, 0, 0, 0, 0, 0]
+        self.gap_fit[7][:] = [-1.22095e-06, -1.5686e-10, -1.09857e-14, 0, 0, 0, 0, 0, 0, 0]
+        self.gap_fit[8][:] = [7.13593e-09, 4.69949e-13, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.gap_fit[9][:] = [-1.74622e-11, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
         self.polphase = xr.load_dataarray(configpath / "polphase.nc")
         self.phasepol = xr.DataArray(
             data=self.polphase.pol,
@@ -343,57 +345,52 @@ class EnPos(PseudoPositioner):
 
         if (pol == -1) and 105 < energy < 1200:
             encalc = energy - 105.002
-            gap = 13979
-            gap += 82.857 * encalc ** 1
-            gap += -0.26294 * encalc ** 2
-            gap += 0.00090199 * encalc ** 3
-            gap += -2.3176e-06 * encalc ** 4
-            gap += 4.205e-09 * encalc ** 5
-            gap += -5.139e-12 * encalc ** 6
-            gap += 4.0034e-15 * encalc ** 7
-            gap += -1.7862e-18 * encalc ** 8
-            gap += 3.4687e-22 * encalc ** 9
-            return max(14000, gap)
+            gap = 13979.0
+            gap +=   82.857      * encalc ** 1
+            gap +=   -0.26294    * encalc ** 2
+            gap +=    0.00090199 * encalc ** 3
+            gap +=   -2.3176e-06 * encalc ** 4
+            gap +=    4.205e-09  * encalc ** 5
+            gap +=   -5.139e-12  * encalc ** 6
+            gap +=    4.0034e-15 * encalc ** 7
+            gap +=   -1.7862e-18 * encalc ** 8
+            gap +=    3.4687e-22 * encalc ** 9
+            return max(14000.0,min(100000.0, gap))
         elif (pol == -0.5) and 105 < energy < 1200:
             encalc = energy - 104.996
-            gap = 14013
-            gap += 82.76 * encalc ** 1
-            gap += -0.26128 * encalc ** 2
-            gap += 0.00088353 * encalc ** 3
-            gap += -2.2149e-06 * encalc ** 4
-            gap += 3.8919e-09 * encalc ** 5
-            gap += -4.5887e-12 * encalc ** 6
-            gap += 3.4467e-15 * encalc ** 7
-            gap += -1.4851e-18 * encalc ** 8
-            gap += 2.795e-22 * encalc ** 9
-            return max(14000, gap)
+            gap = 14013.0
+            gap +=   82.76       * encalc ** 1
+            gap +=   -0.26128    * encalc ** 2
+            gap +=    0.00088353 * encalc ** 3
+            gap +=   -2.2149e-06 * encalc ** 4
+            gap +=    3.8919e-09 * encalc ** 5
+            gap +=   -4.5887e-12 * encalc ** 6
+            gap +=    3.4467e-15 * encalc ** 7
+            gap +=   -1.4851e-18 * encalc ** 8
+            gap +=    2.795e-22  * encalc ** 9
+            return max(14000.0,min(100000.0, gap))
         elif 0 <= pol <= 90:
-            phase = self.phase(energy, pol) / 1000
-            g250_gap = float(self.L250_gap_mrg.interp(Energies=energy, phase=phase))
-            g250_intens = float(
-                self.L250_intens_mrg.interp(Energies=energy, phase=phase)
-            )
-            g1200_gap = float(self.L1200_gap_mrg.interp(Energies=energy, phase=phase))
-            g1200_intens = float(
-                self.L1200_intens_mrg.interp(Energies=energy, phase=phase)
-            )
+            return max(14000.0,min(100000.0, self.epu_gap(energy,pol)))
         elif 90 < pol <= 180:
-            phase = self.phase(energy, pol) / 1000
-            g250_gap = float(self.L250_gap_mrg.interp(Energies=energy, phase=phase))
-            g250_intens = float(
-                self.L250_intens_mrg.interp(Energies=energy, phase=phase)
-            )
-            g1200_gap = float(self.L1200_gap_mrg.interp(Energies=energy, phase=phase))
-            g1200_intens = float(
-                self.L1200_intens_mrg.interp(Energies=energy, phase=phase)
-            )
+            return max(14000.0,min(100000.0, self.epu_gap(energy,180.0-pol)))
         else:
             return np.nan
 
-        if np.isnan(g250_gap):
-            return min(100000, max(14000, g1200_gap))
-        else:
-            return min(100000, max(14000, g250_gap))
+
+    def epu_gap(self, en, pol):
+        """
+        calculate the epu gap from the energy and polarization, using a 2D polynomial fit
+        @param en: energy (valid between ~70 and 1300
+        @param pol: polarization (valid between 0 and 90)
+        @return: gap in microns
+        """
+        x = float(en)
+        y = float(pol)
+        z = 0.0
+        for i in np.arange(self.gap_fit.shape[0]):
+            for j in np.arange(self.gap_fit.shape[1]):
+                z += self.gap_fit[j, i] * (x ** i) * (y ** j)
+        return z
 
     def phase(self, en, pol):
         if pol == -1:
@@ -401,12 +398,9 @@ class EnPos(PseudoPositioner):
         elif pol == -0.5:
             return 15000
         elif 90 < pol <= 180:
-            return -min(29500,max(0,
-                                float(self.polphase.interp(pol=180 - pol, method="cubic"))))
+            return -min(29500.0,max(0.0,float(self.polphase.interp(pol=180 - pol, method="cubic"))))
         else:
-            return min(
-                29500, max(0, float(self.polphase.interp(pol=pol, method="cubic")))
-            )
+            return min(29500.0, max(0.0, float(self.polphase.interp(pol=pol, method="cubic"))))
 
     def pol(self, phase, mode):
         if mode == 0:
@@ -421,6 +415,11 @@ class EnPos(PseudoPositioner):
             )
 
     def mode(self, pol):
+        """
+
+        @param pol:
+        @return:
+        """
         if pol == -1:
             return 0
         elif pol == -0.5:
@@ -458,6 +457,7 @@ class EnPos(PseudoPositioner):
 
     def scanlock_calc(self):
         return self.locked
+
 
 def base_set_polarization(pol, en):
     yield from bps.mv(en.polarization, pol)
