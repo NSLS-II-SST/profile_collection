cc4159e1a15b5bfa5a633eac391e946a633b2cad
maffettone <phillip.maffettone@gmail.com>
2022-04-22 12:16:13 -0400

    Add sys path expansion for packages

--- startup/SST/RSoXS/startup.py
+++ startup/SST/RSoXS/startup.py
@@ -1,3 +1,16 @@
+import sys
+from pathlib import Path
+
+paths = [
+    path
+    for path in Path(
+        "/nsls2/data/sst1/ucal/shared/config/bluesky/collection_packages"
+    ).glob("*")
+    if path.is_dir()
+]
+for path in paths:
+    sys.path.append(str(path))
+
 import os
 import nslsii
 import time
