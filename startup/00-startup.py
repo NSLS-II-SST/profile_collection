5ee9471bf6acd3c646d328080fd16cd20d7f9713
Eliot Gann <37706296+EliotGann@users.noreply.github.com>
2023-01-06 15:32:41 -0500

    changing default imports

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -47,8 +47,7 @@ from rsoxs.Functions.configurations import *
 from rsoxs.Functions.schemas import *
 from rsoxs.Functions.PVdictionary import *
 from rsoxs.Functions.energyscancore import *
-from rsoxs.Functions.energyscans import *
-from rsoxs.Functions.NEXAFSscans import *
+from rsoxs.Functions.rsoxs_plans import *
 from rsoxs.HW.slackbot import rsoxs_bot
 from rsoxs.Functions.acquisitions import *
 from rsoxs.Functions.sample_spreadsheets import *
