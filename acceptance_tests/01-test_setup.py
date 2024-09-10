04d30cb12c6681303d03cc27a4ce8b6ea9196467
PriyankaKetkarBNL <155195376+PriyankaKetkarBNL@users.noreply.github.com>
2024-09-10 16:48:50 -0400

    Delete acceptance_tests directory
    These tests are unnecessary and use old metadata.  They are being removed for now, and might get replaced with more up-to-date tests in a future issue.

--- acceptance_tests/01-test_setup.py
+++
@@ -1,8 +0,0 @@
-from re_setup import testing_RE_md
-
-with testing_RE_md(RE):
-    print("This sample dictionary should be referencing testing.")
-    print(get_sample_dict())
-
-print("This sample dictionary should be referencing the most recent sample.")
-print(get_sample_dict())
